545d7cce181161ecc6554f88abcd5e9b9018338d
Vitaly R <mortiv16@gmail.com>
2020-04-23 17:36:32 +0300

    implemented embedding fetcher

--- FastText.jl
+++ FastText.jl
@@ -1,42 +1,72 @@
-using Flux
+# using Flux
+
+
+
+module FT
 
 include("Vocab.jl")
 using .Vocabulary
 
+export learnVocab!, Vocab, prune,
+        FastText
 
 struct FastText
     in
     out
     bucket
     vocab
+    min_ngram
+    max_ngram
 end
 
-FastText(voc_s::Integer, dim_s::Integer, vocab::Vocab; bucket_size::Integer=2000) = 
-    FastText(
-        rand(voc_s, dim_s), 
-        rand(voc_s, dim_s), 
-        rand(bucket_size, dim_s),
-        vocab
-    )
+# FastText(voc_s::Integer, dim_s::Integer, vocab::Vocab; bucket_size::Integer=2000) = 
+#     FastText(
+#         rand(voc_s, dim_s), 
+#         rand(voc_s, dim_s), 
+#         rand(bucket_size, dim_s),
+#         vocab
+#     )
 
-FastText(vocab::Vocab, dim_s::Integer; bucket_size::Integer=2000) = 
+FastText(vocab::Vocab, 
+        dim_s::Int64;
+        bucket_size::Int64=20000, 
+        min_ngram::Int64=3, 
+        max_ngram::Int64=5) = 
     FastText(
         rand(length(vocab), dim_s), 
         rand(length(vocab), dim_s), 
         rand(bucket_size, dim_s),
-        vocab
+        vocab,
+        min_ngram,
+        max_ngram
     )
 
 Base.getindex(m::FastText, word::String) = begin
-    pieces = in_pieces(word)
-    bucket_idx = hash_pieces(pieces)
-    bucket_emb = sum(m.bucket[bucket_idx, :], dims=1)
-    word_ind = word_lookup(word)
+    pieces = in_pieces(word, m.min_ngram, m.max_ngram)
+    bucket_idx = hash_piece.(pieces, length(m.vocab))
+    bucket_emb = sum(m.bucket[bucket_idx, :], dims=1)[:]
+    word_ind = m.vocab.vocab[word]
     word_emb = m.in[word_ind,:]
 
-    (bucket_emb + word_emb) / (length(bucket_idx) + 1)
+    (bucket_emb + word_emb[:]) / (length(pieces) + 1)
 end
 
-Flux.@functor FastText
+W_BEGIN = "<"
+W_END = ">"
 
+in_pieces(word::String, min_ngram::Integer, max_ngram::Integer) = begin
+    word = W_BEGIN * word * W_END
+    pieces = []
+    for pos in 1:(length(word)-min_ngram), w in min_ngram:max_ngram
+        if pos+w <= length(word)
+            push!(pieces, word[pos:(pos+w)])
+        end
+    end
+    pieces
+end
 
+hash_piece(x, voc_size) = hash(x) % voc_size
+
+# Flux.@functor FastText
+
+end
--- main.jl
+++ main.jl
@@ -1,27 +1,47 @@
 cd("/Users/LTV/dev/FastText.jl/")
 include("FastText.jl")
+include("LanguageTools.jl")
+using .LanguageTools
+using .FT
 
-EMB_SIZE = 15
-VOC_SIZE = 10
-BUCKET_SIZE = 10
 
-input_index = [5, 5, 5, 5]
-output_indices = [1, 2, 3, 5]
+v = Vocab()
 
-data = []
-for i in 1:4
-    push!(data, ([input_index[i], output_indices[i]], 1.))
+for line in eachline(stdin)
+    tokens = tokenize(line)
+    learnVocab!(v, tokens)
 end
 
-ft = FastText(VOC_SIZE, EMB_SIZE, BUCKET_SIZE)
+v = prune(v, 100)
 
-loss(x,y) = Flux.logitbinarycrossentropy(ft.in[x[1]]' * ft.out[x[2]], y)
+@show v
 
-opt = Descent(0.3)
+# ft = FastText(v, 15, bucket_size=20000, min_ngram=3, max_ngram=5)
 
-for _ in 1:100
-    Flux.train!(loss, params(ft), data, opt)
-end
+# @show ft["Schopenhauer"]
+
+
+# EMB_SIZE = 15
+# VOC_SIZE = 10
+# BUCKET_SIZE = 10
+
+# input_index = [5, 5, 5, 5]
+# output_indices = [1, 2, 3, 5]
+
+# data = []
+# for i in 1:4
+#     push!(data, ([input_index[i], output_indices[i]], 1.))
+# end
+
+# ft = FastText(VOC_SIZE, EMB_SIZE, BUCKET_SIZE)
+
+# loss(x,y) = Flux.logitbinarycrossentropy(ft.in[x[1]]' * ft.out[x[2]], y)
+
+# opt = Descent(0.3)
+
+# for _ in 1:100
+#     Flux.train!(loss, params(ft), data, opt)
+# end
 
 
 
